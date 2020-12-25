201688b9f3665396b6959ca53756ed683a66093f
Pavel Burmistrov <pamerus@yandex.ru>
2020-12-26 00:42:47 +0300

    add component

--- todo-list/src/components/mainc/content/content.jsx
+++ todo-list/src/components/mainc/content/content.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import style from "./content.module.css"
 
-const Content = () => {
+const Content = (props) => {
     return(
         <div className={style.content}>
             <p>content</p>
+            {/*for (let i = 0; i < 10; i++)*/}
+
         </div>
     )
 }
---
+++ todo-list/src/components/mainc/content/tasks/tasks.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import style from "./tasks.module.css"
+
+const Tasks = (props) => {
+    return(
+        <div className={style.tasks}>
+            <p>task1</p>
+            <p>task2</p>
+            <p>task3</p>
+        </div>
+    )
+}
+
+export default Content;
